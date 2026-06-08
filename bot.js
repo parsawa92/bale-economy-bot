f16e8a5e807a10b5908c95f2a539b8c2e6639181
parsawa92 <z1342965@gmail.com>
2026-06-08 18:22:07 +0330

    Update bot.js

--- bot.js
+++ bot.js
@@ -663,12 +663,10 @@ ${SHOP[i]}
     }
 );
 
-app.listen(
-    3000,
-    () => {
+const PORT = process.env.PORT || 3000;
 
-        console.log(
-            "BOT ONLINE"
-        );
+app.listen(PORT, () => {
+  console.log("BOT ONLINE");
+});
     }
 );

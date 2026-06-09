949e3ff34c4b4fbfb51206071c69ad03de06b909
parsawa92 <z1342965@gmail.com>
2026-06-09 21:12:14 +0330

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    TOKEN: process.env.TOKEN,
+
+    BIN_ID: process.env.BIN_ID,
+
+    MASTER_KEY: process.env.MASTER_KEY,
+
+    WORK_COOLDOWN: 60000,
+
+    DAILY_COOLDOWN: 86400000
+
+};

a8c7647a767a02325eaf1343dcb4d76937efe95d
parsawa92 <z1342965@gmail.com>
2026-06-09 21:13:33 +0330

    Create database.js

---
+++ database.js
@@ -0,0 +1,29 @@
+const axios = require("axios");
+
+async function loadDB() {
+...
+}
+
+async function saveDB() {
+...
+}
+
+async function getUser() {
+...
+}
+
+async function updateUser() {
+...
+}
+
+async function getTopUsers() {
+...
+}
+
+module.exports = {
+    loadDB,
+    saveDB,
+    getUser,
+    updateUser,
+    getTopUsers
+};
